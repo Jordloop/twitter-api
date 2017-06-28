2cfd92742b8f9bdef50f79efd2ed516f940351d5
Jordan Loop and Joshua Fairchild <jordan+joshua@>
2017-06-28 14:00:54 -0700

    install twit library, add a method that will make an api request and return 10 user tweets

--- js/twitter-api-interface.js
+++ js/twitter-api-interface.js
@@ -1,6 +1,8 @@
 const TwitterApi = require('./../js/twitter-api.js').twitterModule;
+// const twit = require('twit');
+
 
 $(() => {
   const twitAPI = new TwitterApi();
-  
+  $("#output").text(twitAPI.basicSearch('javascript'));
 });
--- js/twitter-api.js
+++ js/twitter-api.js
@@ -1,5 +1,36 @@
-function TwitterApi() {
+const twit = require('twit');
+const consumerKey = require('./../.env').consumerKey;
+const consumerSecret = require('./../.env').consumerSecret;
+const accessToken = require('./../.env').accessToken;
+const accessTokenSecret = require('./../.env').accessTokenSecret;
 
+function TwitterApi() {
+  this.twit = new twit({
+    consumer_key: consumerKey,
+    consumer_secret: consumerSecret,
+    access_token: accessToken,
+    access_token_secret: accessTokenSecret
+  });
 }
 
+TwitterApi.prototype.basicSearch = function(searchQuery) {
+  console.log('hello');
+  this.twit.get('search/tweets', {q: searchQuery, count: 10})
+    .then(response => {
+      response.data.statuses.forEach(status => {
+        console.log(`Status: ${status.text}`)
+      });
+    })
+    .catch(error => console.log('found an error!', error.stack));
+};
+
+
 exports.twitterModule = TwitterApi;
+
+
+
+//execute below
+//--------------------
+const twitAPI = new TwitterApi();
+
+twitAPI.basicSearch('javascript');
